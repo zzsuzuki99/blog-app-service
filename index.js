5e8a4cf3696b5aa897bc2d5d863e43741e493fae
Steven <nguyen.quocnam10a1@gmail.com>
2018-09-23 21:58:19 +0700

    add Access-control-allow-origin

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(bodyParser.json())
 // app.use(multer())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 app.get('/', function (req, res) {
   res.send('Hello world!')
 })
